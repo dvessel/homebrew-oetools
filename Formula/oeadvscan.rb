a906f82bacca75f625ca13afa88814bcbeacc657
Joon Park <dvessel@gmail.com>
2021-01-23 14:18:28 -0500

    oeadvscan 0.9.02 update.

--- Formula/oeadvscan.rb
+++ Formula/oeadvscan.rb
@@ -1,8 +1,8 @@
 class Oeadvscan < Formula
   desc "ROM updater and validation tool for OpenEmu's MAME core."
   homepage "https://github.com/dvessel/oeadvscan"
-  url "https://github.com/dvessel/oeadvscan/archive/v0.9.01.tar.gz"
-  sha256 "ddfa902d2888dc966f58c0e882b8bafdcd4daf29ba336fc2024f4e5d37edfb16"
+  url "https://github.com/dvessel/oeadvscan/archive/v0.9.02.tar.gz"
+  sha256 "ab5829442525d1a66917a93c13b2ae5e0f12fc384dac2b3ede72abad1e839e20"
   license "MIT"
 
   depends_on "advancescan"
